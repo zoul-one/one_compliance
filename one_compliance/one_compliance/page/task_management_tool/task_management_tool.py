c99f3eb72d86bde1b3ed71d737289a3b5385f24e
Jumana-K <jumanahaseen555@gmail.com>
2026-07-14 11:59:50 +0530

    chore:role based view

--- one_compliance/one_compliance/page/task_management_tool/task_management_tool.py
+++ one_compliance/one_compliance/page/task_management_tool/task_management_tool.py
@@ -62,7 +62,7 @@ def get_task(status=None, task=None, project=None, customer=None, department=Non
 		conditions.append("t.exp_end_date < %(to_date)s")
 		values["to_date"] = to_date
 
-	if current_user != "Administrator" and "Executive" in roles:
+	if current_user != "Administrator":
 		conditions.append("(t.readiness_status = 'Ready' OR t.readiness_status IS NULL OR t.readiness_status = '')")
 
 	where_clause = "WHERE " + " AND ".join(conditions) if conditions else ""
